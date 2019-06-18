382f473fe099ae8d8a213d2f77f124fc580b34c4
David Krier <dkflyboy@gmail.com>
2019-06-18 14:31:36 -0500

    updated routs

--- routes/health.js
+++ routes/health.js
@@ -2,6 +2,9 @@
 var express = require('express');
 var router = express.Router();
 
+
+// API's go here..
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('health', { title: 'Express' });
